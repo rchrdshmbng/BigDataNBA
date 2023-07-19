b0987f90e9de8884855663905247e0a742817983
lanceinb <lance.pangan@gmail.com>
2023-07-19 13:02:29 +0200

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -125,7 +125,7 @@ with col3:
     st.write("")
 
 st.sidebar.markdown(" ## About Hooponomics")
-st.sidebar.markdown("The Hooponomics predictive model sorts every active NBA player into one of seven classes that portray their anticipated annual salary if they were to negotiate a fresh contract following the 2021-22 season. This model was developed by using information from the past 5 years, incorporating player statistics and performance indicators. - Team VLR"  )
+st.sidebar.markdown("The Hooponomics predictive model sorts every active NBA player into one of seven classes that portray their anticipated annual salary if they were to negotiate a fresh contract following the 2021-22 season. This model was developed by using information from the past five years, incorporating player statistics and performance indicators. -Team VLR (Vincenzo Lance Richard)"  )
 
 ##########################################
 ## Player Tab                           ##
